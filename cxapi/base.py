49910db6896576b716df6f88b4bffd3cf3fe7446
wuki <51240031+wukibaka@users.noreply.github.com>
2023-11-30 15:17:53 +0800

    Update base.py

--- cxapi/base.py
+++ cxapi/base.py
@@ -12,7 +12,7 @@ from .schema import QuestionModel, QuestionsExportSchema
 from .session import SessionWraper
 
 # SSR页面-客户端章节任务卡片
-PAGE_MOBILE_CHAPTER_CARD = "https://mooc1-api.chaoxing.com/knowledge/cards"
+PAGE_MOBILE_CHAPTER_CARD = "https://mooc.istudy.szpt.edu.cn/knowledge/cards"
 
 
 class QAQDtoBase:
